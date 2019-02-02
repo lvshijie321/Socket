b2d5fd67e3cbb1ac165afd53c59e19d9b8adb7e6
lvshijie <lvshijie@aiwending.com>
2019-02-02 14:39:10 +0800

    UDP发送数据和回送数据

--- src/main/java/lesson_02_UDP/UDPProvider.java
+++ src/main/java/lesson_02_UDP/UDPProvider.java
@@ -4,47 +4,48 @@ package lesson_02_UDP;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.sql.SQLOutput;
 
-/***
- * UDP 提供者， 用于提供服务
+/**
+ * UDP 提供者，用于提供服务
  */
-
 public class UDPProvider {
     public static void main(String[] args) throws IOException {
-        System.out.println("UDDPProvider Started.");
+        System.out.println("UDPProvider Started");
 
         // 作为接收者，指定一个端口用于数据接收
         DatagramSocket ds = new DatagramSocket(20000);
 
-        // 构建接收实体
+        // 构建接收数据包
         final byte[] buf = new byte[512];
+        System.out.println(buf.length);
         DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
 
-        // 接收
+        // 接收（阻塞）
         ds.receive(receivePack);
 
-        // 打印接收到的信息与发送者的信息
-        // 发送者的 IP 地址
-        String ip = receivePack.getAddress().getHostAddress();
-        int port = receivePack.getPort();
-        int dataLen = receivePack.getLength();
+        // 打印接收到的信息和发送者信息
+        String ip = receivePack.getAddress().getHostAddress(); // 发送者的 IP 地址
+        int port = receivePack.getPort(); // 发送者的端口
+        int dataLen = receivePack.getLength(); // 1 个中文字符 3 字节，1 个英文字符 1 字节
+        System.out.println(dataLen);
         String data = new String(receivePack.getData(), 0, dataLen);
+        //System.out.println(receivePack.getData() instanceof byte[] );//true
         System.out.println("UDPProvider receive from ip: " + ip + "\tport: " + port + "\tdata: " + data);
 
-        // 构建一份回送数据
-        String responseData = "Receive data with len: " + dataLen;
+        // 构建一份回送数据包
+        String responseData = "Receive data with len：" + dataLen;
         byte[] responseDataBytes = responseData.getBytes();
-        // 直接根据发送者构建一份回送信息
         DatagramPacket responsePacket = new DatagramPacket(responseDataBytes,
                 responseDataBytes.length,
                 receivePack.getAddress(),
                 receivePack.getPort());
 
+        // 发送
         ds.send(responsePacket);
 
         // 完成
-        System.out.println("UDDPProvider Finished.");
         ds.close();
+        System.out.println("UDPProvider Finished.");
     }
-
 }
--- src/main/java/lesson_02_UDP/UDPSearcher.java
+++ src/main/java/lesson_02_UDP/UDPSearcher.java
@@ -7,13 +7,11 @@ import java.net.InetAddress;
 
 public class UDPSearcher {
     public static void main(String[] args) throws IOException{
-        System.out.println("UDDSearcher Started.");
-
         // 作为搜索方，让系统自动分配端口
         DatagramSocket ds = new DatagramSocket();
 
-        // 构建一份发送数据
-        String requestData = "Hello Word";
+        // 构建一份发送的数据包
+        String requestData = "神州";
         byte[] requestDataBytes = requestData.getBytes();
         DatagramPacket requestPacket = new DatagramPacket(requestDataBytes,
                 requestDataBytes.length);
@@ -23,26 +21,22 @@ public class UDPSearcher {
         // 发送
         ds.send(requestPacket);
 
-
-        // 构建接收实体
+        // 构建接收数据包
         final byte[] buf = new byte[512];
         DatagramPacket receivePack = new DatagramPacket(buf, buf.length);
 
         // 接收
         ds.receive(receivePack);
 
-        // 打印接收到的信息与发送者的信息
-        // 发送者的 IP 地址
-        String ip = receivePack.getAddress().getHostAddress();
+        // 打印接收到的信息和发送者信息
+        String ip = receivePack.getAddress().getHostAddress(); // 发送者的 IP 地址
         int port = receivePack.getPort();
         int dataLen = receivePack.getLength();
         String data = new String(receivePack.getData(), 0, dataLen);
-        System.out.println("UDDSearcher receive from ip: " + ip + "\tport: " + port + "\tdata: " + data);
-
-
+        System.out.println("UDPSearcher receive from ip: " + ip + "\tport: " + port + "\tdata: " + data);
 
         // 完成
-        System.out.println("UDDSearcher Finished.");
+        System.out.println("UDPSearcher Finished.");
         ds.close();
     }
 }
